5ff146f85de973afd1ecceadadfa4a7b6ce9733b
Ian Morton <mortonip@rose-hulman.edu>
2025-01-14 00:00:09 -0500

    DONE

--- relPrime.asm
+++ relPrime.asm
@@ -89,24 +89,23 @@ relPrime:
 	addi	t1, x0, 1
 
 WHILE2:
-	addi	sp, sp, -16
+	addi	sp, sp, -12
 	sw	ra, 0(sp)
 	sw	t0, 4(sp)
 	sw	a0, 8(sp)
-	sw	t2, 12(sp)
 	addi	a1, t0, 0	# a0: n, a1: m
 	jal	ra, gcd
-	addi	t1, a0, 0	# t1: gcd return
+	addi	t2, a0, 0	# t2: gcd return
 	lw	ra, 0(sp)
 	lw	t0, 4(sp)
 	lw	a0, 8(sp)
-	lw	t2, 12(sp)
-	addi	sp, sp, 16
+	addi	sp, sp, 12
 	
 	beq	t1, t2, return_relPrime
 	addi	t0, t0, 1
 	jal	x0, WHILE2
 	
 return_relPrime:
+	addi	a0, t0, 0
 	jalr 	x0, 0(ra)
 	
